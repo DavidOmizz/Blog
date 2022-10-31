5c4c8b9b1916b065f59f5be9ec50429b545e7478
David Omisakin <davidomisakin4good@gmail.com>
2022-10-31 15:32:50 +0100

    done

--- BlogSite/settings.py
+++ BlogSite/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'django-insecure-=7-v!@ra^&bznlj6*@^fo*%$@s+u_gy8)@&)xevleh*w(8ay6s
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
